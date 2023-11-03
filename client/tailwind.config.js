d7f18cee86febccda26d034e8329456a6997a455
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-11-03 19:53:34 -0400

    Create header component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '303a37': '#303a37',
+        '313b3e': '#313b3e',
+        '2e3333': '#2e3333',
+        'c9cdd2': '#c9cdd2',
+        '576f56': '#576f56',
+      }
+    },
   },
   plugins: [],
 }
