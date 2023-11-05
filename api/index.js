e1a1c0b54bcbedf2323b490f582c0d9a47514eec
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-11-04 23:13:11 -0400

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+mongoose.connect(process.env.MONGOD).then(() => {
+    console.log('Connected to MongoDB!');
+    }).catch((err) => {
+        console.log(err);
+    });
+    
 
 const app = express();
 
