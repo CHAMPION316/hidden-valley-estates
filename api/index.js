5e8c35535214dcdff53b757029e9bb94a5a18efb
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-11-08 10:53:38 -0600

    Call userRouter function in routes/index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import userRouter from './routes/user.route.js';
 
 mongoose.connect(process.env.MONGOD).then(() => {
     console.log('Connected to MongoDB!');
@@ -15,3 +16,7 @@ app.listen(3000, () => {
     console.log('Server running on port 3000');
     }
 );
+
+
+// code is to test send/request to client side
+app.use("/api/user", userRouter)
