96180cee572fa4c6f7a278237f456615557ea0b4
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-11-11 11:53:12 -0600

    Create styling for signup page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-2e3333 shadow-md'>
+    <header className='bg-303a37 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className='text-3xl text-center font-semibold my-7 text-teal-600'>Sign Up</h1>
+	  <form className="flex flex-col gap-4">
+		<input type="text" placeholder="username"
+		className="border p-3 rounded-lg" id="username" />
+		<input type="email" placeholder="email"
+		className="border p-3 rounded-lg" id="email" />
+		<input type="password" placeholder="password"
+		className="border p-3 rounded-lg" id="password" />
+		<button className="bg-576f56 text-white p-3 rounded-lg 
+		uppercase hover:opacity-80 disabled:opacity-70">Sign up</button>
+	  </form>
+    </div>
   )
 }
