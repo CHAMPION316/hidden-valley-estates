c557765a4523b1345007cbfc402ffddfd45741d7
Royer Segura-Calderon <royer.seguracalderon@gmail.com>
2023-11-13 17:59:22 -0600

    Fix eslint errors and remove semicolon on export

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,25 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className='text-3xl text-center font-semibold my-7 text-teal-600'>Sign Up</h1>
-	  <form className="flex flex-col gap-4">
-		<input type="text" placeholder="username"
-		className="border p-3 rounded-lg" id="username" />
-		<input type="email" placeholder="email"
-		className="border p-3 rounded-lg" id="email" />
-		<input type="password" placeholder="password"
-		className="border p-3 rounded-lg" id="password" />
-		<button className="bg-576f56 text-white p-3 rounded-lg 
-		uppercase hover:opacity-80 disabled:opacity-70">Sign up</button>
-	  </form>
-	  <div className="flex gap-2 mt-5">
-		<p className="text-white">Have an account?</p>
-		<Link to={"/sign-in"}>
-			<span className="text-teal-600">Sign in</span>
-		</Link>
-	  </div>
+		<h1 className='text-3xl text-center font-semibold my-7 text-teal-600'>Sign Up</h1>
+		<form className="flex flex-col gap-4">
+			<input type="text" placeholder="username"
+			className="border p-3 rounded-lg" id="username" />
+			<input type="email" placeholder="email"
+			className="border p-3 rounded-lg" id="email" />
+			<input type="password" placeholder="password"
+			className="border p-3 rounded-lg" id="password" />
+			<button className="bg-576f56 text-white p-3 rounded-lg 
+			uppercase hover:opacity-80 disabled:opacity-70">Sign up</button>
+		</form>
+		<div className="flex gap-2 mt-5">
+			<p className="text-white">Have an account?</p>
+			<Link to={"/sign-in"}>
+				<span className="text-teal-600">Sign in</span>
+			</Link>
+		</div>
     </div>
   )
 }
